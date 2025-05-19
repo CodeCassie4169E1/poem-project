83901f95d9b7f9a974d557dd7b1a7ae27d5c0bcd
CodeCassie4169E1 <clipcassie@yahoo.com>
2025-05-19 11:21:33 -0500

    AI api addition commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 function generatePoem(event) {
   event.preventDefault();
+  let userInput = document.querySelector("#user-word");
+  let apiKey = "e833efeaaa0b3890o88a585t36443235";
+  let prompt = `Generate a poem about ${userInput.value}`;
+  let context =
+    "You are a funny poem expert and love to write very short poems. Use a four line maxumim layout with rhyming words. Add as many emojis as possible ";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter("#poem", {
-    strings: "Poem",
-    autoStart: true,
-  });
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form");
