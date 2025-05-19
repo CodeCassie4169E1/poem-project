7f4923c167a5542fcf3935d43cc3e8720620c1e3
CodeCassie4169E1 <clipcassie@yahoo.com>
2025-05-19 10:22:31 -0500

    basic JavaScript commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Poem",
+    autoStart: true,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
